6f31e778f62b5ce13d4e30bb77ab0ab32c456b0f
prashanthbilla <85600714+prashanthbilla@users.noreply.github.com>
2022-04-22 12:51:47 +0530

    Adding Backend Spring boot file

---
+++ s3-file-storage-example/src/main/java/com/billa/s3/service/StorageService.java
@@ -0,0 +1,79 @@
+package com.billa.s3.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.util.IOUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+public class StorageService {
+
+    @Value("${application.bucket.name}")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    public String uploadFile(MultipartFile file) {
+        File fileObj = convertMultiPartFileToFile(file);
+        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        s3Client.putObject(new PutObjectRequest(bucketName, fileName, fileObj));
+        fileObj.delete();
+        return "File uploaded : " + fileName;
+    }
+
+
+    public byte[] downloadFile(String fileName) {
+        S3Object s3Object = s3Client.getObject(bucketName, fileName);
+        S3ObjectInputStream inputStream = s3Object.getObjectContent();
+        try {
+            return IOUtils.toByteArray(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<String> getAllObjects() {
+        ListObjectsV2Result list = s3Client.listObjectsV2(bucketName);
+        List<S3ObjectSummary> summaries = list.getObjectSummaries();
+        List<String> keyObjectsAll = new ArrayList<>();
+        for (S3ObjectSummary summary : summaries) {
+            keyObjectsAll.add(summary.getKey());
+        }
+        return keyObjectsAll;
+    }
+
+
+    public String deleteFile(String fileName) {
+        try {
+            s3Client.getObject(bucketName, fileName);
+            s3Client.deleteObject(bucketName, fileName);
+            return fileName + " Deleted ...";
+        }catch (AmazonS3Exception e){
+            return e.getMessage();
+        }
+    }
+
+
+    private File convertMultiPartFileToFile(MultipartFile file) {
+        File convertedFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        } catch (IOException e) {
+            log.error("Error converting multipartFile to file", e);
+        }
+        return convertedFile;
+    }
+}
